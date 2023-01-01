a6b917c52b266796a906ca4a216a7f290e8b6a08
rawandbakr <rawandbakr82@gmail.com>
2023-01-01 18:12:24 +0300

    modified:   frontend/src/components/Cards.js

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -3,9 +3,8 @@ import React from 'react'
 
 export default function Cards(props) {
   return (
-    <div className=' h-screen gap-5 flex flex-wrap '>
+    <div className=' h-screen grid lg:grid-cols-4  md:grid-cols-3  sm:grid-cols-2 m-5'>
       {props.children}    
       </div>
   )
 }
-// grid lg:grid-cols-4  md:grid-cols-3  sm:grid-cols-2 m-5
